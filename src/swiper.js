8aa4e293ce1cf86599d4383f714dd352a52e1379
si-mew-ji <124511094+simyoonji@users.noreply.github.com>
2024-01-03 19:46:03 +0900

    main index  완성!

---
+++ src/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    debugger: true,
+    centeredSlides: false,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
