d422f5d8d0ab12b8d614d963be35f3f85df9e9f9
si-mew-ji <124511094+simyoonji@users.noreply.github.com>
2024-01-09 22:08:00 +0900

    cmcm

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
-$(function(){
-    $(".affiliate__list").mouseover(function(){
+// section affiliate toggle
+function affSlide(){
+    $(".affiliate__list").on("mouseover",function(){
         $("#affiliate").addClass("active");
     });
-    $(".affiliate__list").mouseleave(function(){
+    $(".affiliate__list").on("mouseleave",function(){
         $("#affiliate").removeClass("active");
+    });
+}
+
+$(function(){
+    $(window).resize(function(){
+        if($(window).width() > 1060) {
+            affSlide().on;
+            
+        }else if($(window).width() <= 1060){
+            affSlide().off;
+        }
     });
 });
 
-
-$(".company__inserted > ul > li").click(function(){
-    $(this).find('.inserted_list').slideToggle();
+// footer inserted toggle
+$(function(){
+    $(".inserted-m > li > h6").on("click",function(){
+        $(this).next("ul").toggleClass("slide");
+        $(this).parent("li").siblings("li").children("ul").removeClass("slide");
+    });
 });
+
