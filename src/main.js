73cb646915100d95abb5e7b59c8b545c491f876e
si-mew-ji <124511094+simyoonji@users.noreply.github.com>
2024-01-12 19:02:29 +0900

    media

--- src/main.js
+++ src/main.js
@@ -1,29 +1,46 @@
-// section affiliate toggle
-function affSlide(){
-    $(".affiliate__list").on("mouseover",function(){
-        $("#affiliate").addClass("active");
-    });
-    $(".affiliate__list").on("mouseleave",function(){
-        $("#affiliate").removeClass("active");
-    });
-}
-
 $(function(){
-    $(window).resize(function(){
-        if($(window).width() > 1060) {
-            affSlide().on;
-            
-        }else if($(window).width() <= 1060){
-            affSlide().off;
-        }
-    });
-});
+    // affiliate list show/hide
+    let $accListTitle = $('.acc__list-pc > a');
+    let $accListSub = $('.acc__list_sub-pc');
+    let $affMain = $('.aff__main');
+    let ON = 'on';
 
-// footer inserted toggle
-$(function(){
-    $(".inserted-m > li > h6").on("click",function(){
-        $(this).next("ul").toggleClass("slide");
-        $(this).parent("li").siblings("li").children("ul").removeClass("slide");
+    $(function(){
+        $($accListTitle).on('click',function(){
+            $(this).next($accListSub).toggleClass(ON);
+            $(this).parent('li').siblings('li').children($accListSub).removeClass(ON);
+            $(this).parent('li').toggleClass(ON).siblings('li').removeClass(ON);
+            // 
+            if($accListSub.hasClass(ON)){
+                $affMain.stop().addClass(ON);
+            }else {
+                $affMain.stop().removeClass(ON);
+            }
+        });
     });
-});
+
+    // inserted list toggle
+    let $insertList = $('.inserted > li > h6');
+    let SLIDE = 'slide';
+
+    $(function(){
+        $($insertList).on("click",function(){
+            $(this).next("ul").toggleClass(SLIDE);
+            $(this).parent("li").siblings("li").children("ul").removeClass(SLIDE);
+        });
+    });
+
+   // footer contact slide
+    let $ctList = $('.contact__lf ul > li');
+    let $ctListHide = $('.contact__lf ul > li:nth-of-type(n+2)');
+    let $ctRight = $('.contact__rt');
+
+   $(function(){
+    $($ctList).eq(0).on("click",function(){
+        $($ctListHide).stop().toggleClass(ON);
+        $($ctRight).stop().toggleClass(ON);
+    });
+   });
+
+}); //FN
 
