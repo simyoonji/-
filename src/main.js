78c072f703cd0bfca311eaf9904137dd31895f1e
si-mew-ji <124511094+simyoonji@users.noreply.github.com>
2024-01-17 18:27:31 +0900

    완성

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 $(function(){
     // affiliate list show/hide
-    let $accListTitle = $('.acc__list-pc > a');
-    let $accListSub = $('.acc__list_sub-pc');
-    let $affMain = $('.aff__main');
-    let ON = 'on';
+    const $accListTitle = $('.acc__list-pc > a');
+    const $accListSub = $('.acc__list_sub-pc');
+    const $affMain = $('.aff__main');
+    const CHANGE = 'change';
+    const ON = 'on';
 
     $(function(){
         $($accListTitle).on('click',function(){
             $(this).next($accListSub).toggleClass(ON);
             $(this).parent('li').siblings('li').children($accListSub).removeClass(ON);
             $(this).parent('li').toggleClass(ON).siblings('li').removeClass(ON);
+
+            $(this).toggleClass(CHANGE);
+            $(this).parent('li').siblings('li').children('a').removeClass(CHANGE);
             // 
             if($accListSub.hasClass(ON)){
                 $affMain.stop().addClass(ON);
@@ -18,29 +22,5 @@ $(function(){
             }
         });
     });
-
-    // inserted list toggle
-    let $insertList = $('.inserted > li > h6');
-    let SLIDE = 'slide';
-
-    $(function(){
-        $($insertList).on("click",function(){
-            $(this).next("ul").toggleClass(SLIDE);
-            $(this).parent("li").siblings("li").children("ul").removeClass(SLIDE);
-        });
-    });
-
-   // footer contact slide
-    let $ctList = $('.contact__lf ul > li');
-    let $ctListHide = $('.contact__lf ul > li:nth-of-type(n+2)');
-    let $ctRight = $('.contact__rt');
-
-   $(function(){
-    $($ctList).eq(0).on("click",function(){
-        $($ctListHide).stop().toggleClass(ON);
-        $($ctRight).stop().toggleClass(ON);
-    });
-   });
-
 }); //FN
 
